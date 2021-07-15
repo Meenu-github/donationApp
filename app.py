19b8e6cec817caf67bf0ce1f7eb29cce0a1a0c32
KaviyaGopi <kaviyagopi002@gmail.com>
2021-07-15 09:23:11 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from donationApp import bookDonation, bloodDonation, foodDonation, firstpage
+
+st.markdown("## Welcome to the Donation page.")
+st.title("Donation")
+st.header("Let us come together and donate something for the needy.")
+choice = st.radio("Navigation",["Book Donation", "Blood Donation","Food Donation", "None"])
+if choice=="Book Donation":
+    bookDonation.bookdonate()
+if choice=="Blood Donation" :
+    bloodDonation.bloodDonate()
+if choice=="Food Donation":
+    foodDonation.foodDonate()
+if choice == "None" :
+    st.text("DO YOU KNOW ?")
+    st.text("HOW OLD THIS WEB PAGE IS ?")
+    st.text("THIS WEB PAGE IS MADE ON 14 JULY 2021")
