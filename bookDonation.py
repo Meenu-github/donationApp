1bde0657227ca72a4e3bc705ea5b274163faea01
KaviyaGopi <kaviyagopi002@gmail.com>
2021-07-15 12:22:58 +0530

    food donation

--- bookDonation.py
+++ bookDonation.py
@@ -1,9 +1,6 @@
 import streamlit as st
 def bookdonate() :
     st.markdown("BOOK DONATION")
-    from PIL import Image
-    image = Image.open("E:\Meenu\Snapchat\BookDonation.jpg")
-    st.image(image, caption='Book Donation')
     st.title("Welcome to the book donation page, your old book can bring light in someones future.\nCome let us donate books for needy one.\nYou don't have to walk and donate it you just have to register yourself and we will pick the book from your house address that will be provided.")
     st.write("Here if you are willing to donate book\n you have to register yourself.")
     with st.form(key="Registration for Blood Donation"):
