017735a6b7ea0d220ccedf11d656ef119f056594
“Meenu <meenakshi15sept2000@gmail.com>
2021-07-19 20:36:02 +0530

    Uploaded app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from donationApp import bookDonation, bloodDonation, foodDonation, firstpage, loginPage
+st.title("Donation")
+st.markdown("## Welcome to the Donation page.")
+
+st.header("Let us come together and donate something for the needy.")
+menu = ["None", "Book Donation", "Blood Donation","Food Donation","Organization Login/Register"]
+
+choice = st.selectbox("Navigation", menu)
+if choice=="Book Donation":
+    bookDonation.bookdonate()
+if choice=="Blood Donation" :
+    bloodDonation.bloodDonate()
+if choice=="Food Donation" :
+    foodDonation.foodDonate()
+if choice == "None" :
+    firstpage.firstpage()
+    st.text("DO YOU KNOW ?")
+    st.text("HOW OLD THIS WEB PAGE IS ?")
+    st.text("THIS WEB PAGE IS MADE ON 14 JULY 2021")
+if choice== "Organization Login/Register":
+    loginPage.loginPages()
+    
