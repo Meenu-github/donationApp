660f583cbf17a25231f5900db1a3a9b78d929675
“Meenu <meenakshi15sept2000@gmail.com>
2021-07-24 19:36:31 +0530

    update

--- donationApp/loginPage.py
+++ donationApp/loginPage.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import openpyxl as pxl
 import pandas as pd
+import gspread
+from gspread_dataframe import get_as_dataframe 
 #proj = pd.read_excel('proj.xlsx')
 exp = pxl.load_workbook('proj.xlsx')
 sheet = exp.active
@@ -15,16 +17,25 @@ def loginPages():
             email = st.text_input("E-mail Id")
             password = st.text_input("Password")
             conf_password = st.text_input('Confirm your password')
+            role = ['Hospital','Food Distributor','Orphanage']
+            selectRole = st.selectbox("Role",role)
+            if password== conf_password:
+                pass
+            else:
+                st.info("password do not match with confirm password")
+            
         
             sheet.cell(row=maxrow,column=1).value = userName
             sheet.cell(row=maxrow,column=2).value = email
             sheet.cell(row=maxrow,column=3).value = password
+            sheet.cell(row=maxrow, column=4).value = selectRole
             
 
             submissionButton = st.form_submit_button(label="Sign up")
             if submissionButton==True:
                 exp.save('proj.xlsx')
                 st.success("Successfully sign up")
+                type(selectRole)
 
 
     if page=='Login':
@@ -34,13 +45,64 @@ def loginPages():
             password = st.text_input("Password")
             submissionButton = st.form_submit_button(label="Login")
             if submissionButton==True:
-                proj = pd.read_excel('proj.xlsx',index_col=2)
-                for z in range(maxrow):
-                    
-                    if userName in proj:
-                        pas = pd.read_excel('proj.xlsx',index_col=3)
-                        if password in pas:
-                            st.success('Data found')
+                
+                for i in range(2, sheet.max_row):
+                    if((sheet.cell(row=i, column=2).value == userName)):
+                        if((sheet.cell(row=i, column=3).value == password)):
+                            y = sheet.cell(row=i,column=4).value
+                            st.success('Successfully Login')
+                            type(y)
+                 
+                        else:
+                            st.error("either username or password is incorrect")
+def type(selectRole):
+    if selectRole=='Hospital':
+        exp = pxl.load_workbook('BloodDonation.xlsx')
+        sheet = exp.active
+        maxrow= sheet.max_row+1
+        a2 = ['Yes','No']
+        b2 = st.radio('Do you want data of blood donor :',a2)
+        if b2=='Yes':
+            for i in range(1,sheet.max_row+1):
+                for j in range(1,sheet.max_column+1):
+                    st.text(print(sheet.cell(row=i,column=j).value, end="  "))
+                st.text(print("\n"))
+        if b2=='No':
+            st.info('YOU SELECTED NO.')
 
-                st.success("Successfully login")
+    if selectRole=='Food Distributor':
+        exp = pxl.load_workbook('FoodDonation.xlsx')
+        sheet = exp.active
+        maxrow= sheet.max_row+1
+        a = ['Yes','No']
+        b = st.radio('Do you want data of food donor :',a)
+        if b=='Yes':
+            for i in range(1,sheet.max_row+1):
+                for j in range(1,sheet.max_column+1):
+                    st.text(print(sheet.cell(row=i,column=j).value, end="  "))
+                st.text(print("\n"))
+        if b=='No':
+            st.info('YOU SELECTED NO.')
+
+    if selectRole=='Orphanage':
+        exp = pxl.load_workbook('BookDonation.xlsx')
+        sheet = exp.active
+        maxrow= sheet.max_row+1
+        a1 = ['Yes','No']
+        b1 = st.radio('Do you want data of food donor :',a1)
+        if b1=='Yes':
+            for i in range(1,sheet.max_row+1):
+                for j in range(1,sheet.max_column+1):
+                    st.write(sheet.cell(row=i,column=j).value, end="  ")
+                st.write("\n")
+        if b1=='No':
+            st.info('YOU SELECTED NO.')
+
+                    
+df = get_as_dataframe(sheet)
+st.write(df)
+st.table(df)
+                            
+
+                
 
